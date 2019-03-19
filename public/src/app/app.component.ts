87f889fc0f6fba316d226cd6c538095b9c3fabe1
Raymond Xu <raymondxu92@gmail.com>
2019-03-19 15:10:21 -0700

    Restful Tasks DOM manipulation

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
@@ -6,7 +6,59 @@ import { HttpService } from './http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Restful Task API';
+  tasks: any = [];
+  task: any = [];
+  // task = {
+  //   title: '',
+  //   description: ''
+  // };
+
   constructor(private _httpService: HttpService){}
+    ngOnInit(){
+      this.getTasksFromService();
+      this.getOneTask1FromService();
+      this.getOneTask2FromService();
+      this.getOneTask3FromService();
+      this.getOneTask4FromService();
+    }
+    getTasksFromService() {
+      console.log('fired');
+      let observable = this._httpService.getTasks();
+      observable.subscribe(data => {
+        console.log("Got our tasks!", data);
+        this.tasks = data;
+        // console.log(data['title']); 
+      })
+    }
+    getOneTask1FromService() {
+      
+      let observable1 = this._httpService.getOneTask1();
+      observable1.subscribe(data => {
+        console.log("Got one task!", data);
+        this.task = data;
+      })
+    }
+    getOneTask2FromService() {
+      let observable2 = this._httpService.getOneTask2();
+      observable2.subscribe(data => {
+        console.log("Got another task!", data);
+        // this.task = data['task'];
+      })
+    }
+    getOneTask3FromService() {
+      let observable3 = this._httpService.getOneTask3();
+      observable3.subscribe(data => {
+        console.log("Got another task!", data);
+        // this.task = data['task'];
+      })
+    }
+    getOneTask4FromService() {
+      let observable4 = this._httpService.getOneTask4();
+      observable4.subscribe(data => {
+        console.log("Got another task!", data);
+        // this.task = data['task'];
+      })
+    }
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -1,29 +1,52 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
   export class HttpService {
     constructor(private _http: HttpClient) {
-      this.getTasks();
-      this.getOneTask1();
-      this.getOneTask2();
+      // this.getTasks();
+      // this.getOneTask1();
+      // this.getOneTask2();
+      // this.getOneTask3();
+      // this.getOneTask4();
   }
   
   getTasks() {
-    let tempObservable = this._http.get('/api/tasks');
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    // let tempObservable = this._http.get('/api/tasks');
+    // tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get('/api/tasks');
+  }
+
+  createNewTask(data: any) {
+    return this._http.post('/api/tasks', data);
   }
 
   getOneTask1() {
-    let ID = '5c8b36a5adf80f6c15bcc140';
-    let taskObservable = this._http.get('/api/tasks/'+ID);
-    taskObservable.subscribe(data => console.log("Got one task!", data));
+    let ID1 = '5c8b39f7348add6c448cb2df';
+    // let task1Observable = this._http.get('/api/tasks/'+ID1);
+    // task1Observable.subscribe(data => console.log("Got one task!", data));
+    return this._http.get('api/tasks/'+ID1);
   }
 
   getOneTask2() {
-    let ID1 = '5c8b39f7348add6c448cb2df';
-    let task1Observable = this._http.get('/api/tasks/'+ID1);
-    task1Observable.subscribe(data => console.log("Got the other task!", data));
+    let ID2 = '5c907862db8e792b40526ce6';
+    // let task2Observable = this._http.get('/api/tasks/'+ID2);
+    // task2Observable.subscribe(data => console.log("Got the other task!", data));
+    return this._http.get('/api/tasks/'+ID2);
+  }
+  getOneTask3() {
+    let ID3 = '5c90787cdb8e792b40526ce7';
+    // let task3Observable = this._http.get('/api/tasks/'+ID3);
+    // task3Observable.subscribe(data => console.log("Got the other task!", data));
+    return this._http.get('/api/tasks/'+ID3);
+  }
+  getOneTask4() {
+    let ID4 = '5c9078badb8e792b40526ce8';
+    // let task4Observable = this._http.get('/api/tasks/'+ID4);
+    // task4Observable.subscribe(data => console.log("Got the other task!", data));
+    return this._http.get('/api/tasks/'+ID4);
   }
 }
 
